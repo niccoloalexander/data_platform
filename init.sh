9cb5324f1c315fbc9193ef07ece522c3456617fa
niccoloalexander <nahamlin@gmail.com>
2024-04-12 01:00:42 +0800

    Clean the init script. Use DBT build and chain commands.

--- init.sh
+++ init.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-sleep 10
-
 echo "Executing init.sh script..."
 
 # Install psycopg2
-pip install psycopg2-binary
-
+pip install psycopg2-binary &&
 echo "Psycopg2 installed successfully."
 
-sleep 10
+# Install dbt deps 
+dbt deps &&
+echo "DBT deps installed successfully."
 
 # Run dbt commands
-dbt seed
-sleep 5
-dbt run
-sleep 60
-dbt docs generate
-sleep 10
-dbt docs serve &
-
-echo "dbt commands executed successfully."
-
-sleep 10
+dbt build &&
+echo "DBT project built successfully."
 
 # Run Python script
-python /usr/app/alert_script.py
-
+python /usr/app/alert_script.py &&
 echo "Alerts execution completed."
 
-sleep 3600
+sleep infinity
