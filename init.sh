ae39f3b9ce3947a84f4ba3a85941b96c0ac358f4
niccoloalexander <nahamlin@gmail.com>
2024-05-09 17:25:52 +0800

    Allow errors in init for dev

--- init.sh
+++ init.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
-echo "Executing init.sh script..."
+echo "Executing init.sh script..." &&
 
 # Install psycopg2
 pip install psycopg2-binary &&
-echo "Psycopg2 installed successfully."
+echo "Psycopg2 installed successfully." &&
 
 # Install dbt deps 
 dbt deps &&
-echo "DBT deps installed successfully."
+echo "DBT deps installed successfully." &&
 
 # Run dbt commands
-dbt build &&
-echo "DBT project built successfully."
+dbt build || true
+echo "DBT project built successfully." &&
 
 # Run Python script
-python /usr/app/alert_script.py &&
-echo "Alerts execution completed."
+python /usr/app/alert_script.py || true
+echo "Alerts execution completed." &&
 
 sleep infinity
